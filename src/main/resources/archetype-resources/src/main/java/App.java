a96ff09ec5445b4284c76cb11f821a1a1623defb
Luciano Gonzalez Jensen <luciano1231@hotmail.com>
2015-01-24 20:11:11 -0300

    First maven

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.Luciano;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
